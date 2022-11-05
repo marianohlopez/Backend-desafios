49ac0033e4bfcd34da4aa0b49cdb7b2f0f7ce69b
mlopez <ml.3012@gmail.com>
2022-11-05 14:40:56 -0300

    desafio2

---
+++ desafio-2/contenedor.js
@@ -0,0 +1,96 @@
+const fs = require('fs')
+
+class Contenedor {
+    constructor(archivo) {
+        this.archivo = archivo;
+        this.productos = [];
+    }
+
+    //GUARDAR PRODUCTO NUEVO
+
+    async save(object) {
+
+        let newId
+        
+        const response = await this.getAll()
+
+        //PRODUCTO NO SE REPITE
+
+        if(response.length > 1 && response.some((el) => el.title === object.title)){
+            console.log("el producto ya se encuentra en el catalogo");
+            return
+        }
+        else if(response.length === 0) {
+            newId = 1
+        }
+        else {
+            newId = response[response.length - 1].id + 1
+        }
+
+        const newObject = {...object, id: newId};
+
+        response.push(newObject);
+        
+        try {
+            await fs.promises.writeFile(this.archivo, JSON.stringify(response, null, 2))
+            return newId
+        }
+        catch(err) {
+            throw new Error(`Error al guardar un nuevo objeto: ${err}`)
+        }
+    }
+
+    //OBTENEMOS PRODUCTO BUSCADO MEDIANTE EL ID
+
+    async getById(id) {
+        let object
+        try {
+            const response = await this.getAll()
+            return response.find(item => item.id === id) ?? null
+        }
+        catch(err) {
+            throw new Error(`No se encuentra el producto: ${err}`)
+        }
+    }
+
+    //OBTENGO TODOS LOS PRODUCTOS O UN ARRAY VACIO
+
+    async getAll() {
+        try {
+            const response = await fs.promises.readFile(this.archivo, 'utf-8')
+            return JSON.parse(response)
+        }
+        catch(err) {
+            return this.productos;
+        }
+    }
+
+    async deleteById(id) {
+            const response = await this.getAll();
+            const newResponse = response.filter((item) => item.id !== id)
+        try {
+            await fs.promises.writeFile(this.archivo, `${JSON.stringify(newResponse, null, 2)}`)
+            console.log('producto borrado');
+        }
+        catch(err) {
+            throw new Error(`Error al borrar data: ${err}`)
+        }
+    }
+
+    async deleteAll(){
+        try {
+            await fs.promises.writeFile(this.archivo, `${JSON.stringify([])}`)
+        }
+        catch(err) {
+            throw new Error(`Error al escribir: ${err}`)
+        }
+    }
+}
+
+module.exports = Contenedor
+
+
+
+
+
+
---
+++ desafio-2/test.js
@@ -0,0 +1,58 @@
+const Contenedor = require('./contenedor.js');
+
+const item1 = {
+    title: "Escuadra",
+    price: 123.45,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+}
+
+const item2 = {
+    title: "Calculadora",
+    price: 234.56,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+}
+  
+const item3 ={
+    title: "Globo Terráqueo",
+    price: 345.67,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+}
+
+const item4 = {
+    title: "Escuadra",
+    price: 12343,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+}
+
+const contenedor = new Contenedor('./productos.txt');
+
+const test = async () => {
+
+    let datos1 = await contenedor.getAll()
+    console.log(datos1)
+
+    let save1 = await contenedor.save(item1)
+    console.log(save1);
+    let save2 = await contenedor.save(item2)
+    console.log(save2);
+    let save3 = await contenedor.save(item3)
+    console.log(save3);
+
+    let datos2 = await contenedor.getAll()
+    console.log(datos2)
+
+    let busca1 = await contenedor.getById(1)
+    console.log(busca1)
+
+    let busca2 = await contenedor.getById(10)
+    console.log(busca2)
+
+    await contenedor.deleteById(1)
+    let delete1 = await contenedor.getAll()
+    console.log(delete1)
+
+    await contenedor.deleteAll()
+    let delete2 = await contenedor.getAll()
+    console.log(delete2)
+}
+test()
